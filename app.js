337e17ab3b35605ba7a5ee8d76d48162dfc17c3b
Konain1 <kaunainkasab@gmail.com>
2023-10-16 00:31:52 +0530

    todoDone

--- app.js
+++ app.js
@@ -1,2 +1,63 @@
 
 const form = document.querySelector('.form')
+const inputText = document.getElementById('inputText')
+const addbtn = document.querySelector('.addbtn')
+const taskDiv = document.querySelector('.taskDiv')
+const task = document.querySelector('.task')
+
+
+// events
+
+addbtn.addEventListener('click',(e)=>{
+    e.preventDefault()
+
+    let data = inputText.value;
+
+    if(data === ''){
+        let pre = document.createElement('p')
+        pre.innerHTML = 'Do not make empty task'
+
+        form.appendChild(pre)
+        setTimeout(() => {
+            pre.innerHTML = ''
+        }, 3000);
+      
+    }else{
+
+        console.log(data)
+        createTask(data)
+    }
+
+})
+
+function createTask(data){
+    let newTask = document.createElement('div')
+    newTask.className = 'task'
+    newTask.innerHTML = `
+    
+    <span>${data}</span>
+    <div class="icons">
+        <i class="fa-solid fa-pen-to-square editbtn"></i>
+        <i class="fa-solid fa-trash deletebtn"></i>
+    </div>
+
+    `;
+
+    taskDiv.appendChild(newTask)
+
+    const deletebtn = newTask.querySelector('.deletebtn');
+    const editbtn = newTask.querySelector('.editbtn');
+
+    deletebtn.addEventListener('click',()=>{
+   
+        deletebtn.parentElement.parentElement.remove()
+    })
+
+    editbtn.addEventListener('click',()=>{
+
+       let val = newTask.querySelector('span').innerHTML
+       inputText.value = val
+       newTask.remove()
+    })
+}
+
